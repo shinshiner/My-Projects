721a34d0cbc00d01172bf41fd869feb89003eccd
shin <qiongwuhero@163.com>
2016-12-24 09:37:16 +0800

    测试程序用的头文件
    RT

---
+++ STL vector/class-integer.hpp
@@ -0,0 +1,13 @@
+#pragma once
+class Integer {
+private:
+	int data;
+public:
+	Integer(const int &value) : data(value) {}
+	Integer(const Integer &other) : data(other.data) {}
+	bool operator==(const Integer &t)
+	{
+		return data == t.data;
+	}
+};
+
