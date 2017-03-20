7cd45e725f85aac9ab307cfebe85d3add9a4baa3
ZeLin.Ye_SJTU_MVIG_training <qiongwuhero@163.com>
2017-03-20 19:22:07 +0800

    主实现部分

--- Bit Operations for Integer and Floating Numbers/bits.c
+++ Bit Operations for Integer and Floating Numbers/bits.c
@@ -1,7 +1,6 @@
 /* 
  *Lab of Computer Architecture 
  * 
- * <Please put your name and userid here>
  * 
  * bits.c - Source file with your solutions to the Lab.
  *          This is the file you will hand in to your instructor.
